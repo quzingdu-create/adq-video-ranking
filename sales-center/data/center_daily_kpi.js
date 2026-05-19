14a3d05254d7276a2f60657025bdc1fdd872b042
duziqing <duziqing@users.noreply.github.com>
2026-05-19 13:11:48 +0800

    data refresh 2026-05-18 (T-1)

--- sales-center/data/center_daily_kpi.js
+++ sales-center/data/center_daily_kpi.js
@@ -1,2 +1,2 @@
-/* Auto-generated 2026-05-15 (T-1=2026-05-17) */
-window.__CENTER_DAILY_KPI__ = {"dataDate":"2026-05-17","centerYestCost":8205635.4,"centerPrevCost":7921097.3,"centerDayCostRate":0.0359,"centerCost":8205635.4,"centerCostChg":0.0359,"updatedAt":"2026-05-18 10:35","source":"https://adata.woa.com/bi/view/15729?s=E0fBs","q2NewCount":4520,"q2ValidCount":1135,"q2RisingCount":25,"q2NewYestCost":1212705.9,"q2NewDayCostRate":0.2129,"q1NewCount":7160,"q1ValidCount":1784,"q1RisingCount":45};
+/* Auto-generated 2026-05-15 (T-1=2026-05-18) */
+window.__CENTER_DAILY_KPI__ = {"dataDate":"2026-05-18","centerYestCost":6399260.7,"centerPrevCost":8215194.4,"centerDayCostRate":-0.221,"centerCost":6399260.7,"centerCostChg":-0.221,"updatedAt":"2026-05-19 13:11","source":"https://adata.woa.com/bi/view/15729?s=E0fBs","q2NewCount":4598,"q2ValidCount":1162,"q2RisingCount":25,"q2NewYestCost":1088252.4,"q2NewDayCostRate":-0.1079,"q1NewCount":7160,"q1ValidCount":1784,"q1RisingCount":45};
